81baa4c787e43f5143d3fef04fce02cace8ed314
manassingh14 <manojsinghh196999@gmail.com>
2025-08-05 11:52:27 +0530

    Initial commit

---
+++ backend/src/routes/auth_routes.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { login, logout, signup } from "../controller/auth_controlle.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { updateProfile } from "../controller/auth_controlle.js";
+import { checkAuth } from "../controller/auth_controlle.js";
+const router =express.Router();
+router.post("/signup",signup);
+
+router.post("/login",login);
+
+router.post("/logout",logout);
+router.put("/update-profile", protectRoute, updateProfile);
+router.get("/check",protectRoute,checkAuth)
+export default router;
