35a8e7b7695dd5282cfa46974de73e79030ed164
manassingh14 <manojsinghh196999@gmail.com>
2025-12-19 16:02:31 +0530

    project is ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,11 @@ import authRoutes from "./routes/auth_routes.js";
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import path from "path";
 import { app,server } from "./lib/socket.js";
 dotenv.config()
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
@@ -15,7 +17,14 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes);
+app.use("/api/messages",messageRoutes);]
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"..","frontend","dist")));   
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"..","frontend","dist","index.html"));
+    }); 
+}
 
 
 server.listen(PORT,() => {
